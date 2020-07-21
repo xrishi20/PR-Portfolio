f81f7facb8ac7b13ac77af1f6b48982671d71084
xrishi20 <xrishi20@gmail.com>
2020-07-21 13:59:50 +0530

    mobile test

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        "myYellow": "#FBFF12",
+        "myTeal": "#41EAD4",
+        "myPink": "#FF206E",
+        "myBlack": "#0C0F0A",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
